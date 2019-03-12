70e442a716ea6edf72ee147f8506e9f5540df619
Seweryn91 <s.talaj@gmail.com>
2019-03-12 16:49:19 +0100

    Create recursive flattener methods

--- src/main/java/Flattener.java
+++ src/main/java/Flattener.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Flattener {
+
+    private List<Object> flat = new ArrayList<>();
+
+    @SuppressWarnings("unchecked")
+    List<Object> flattenRecursively(List<Object> multidimensionalList) {
+        for (Object object : multidimensionalList) {
+            if (object instanceof List)
+            flatten( (List<Object>) object);
+            else flat.add(object);
+        }
+        return flat;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void flatten(Object o) {
+        if (o instanceof List) {
+            flatten((List) o);
+        }
+    }
+
+
+    private void flatten(List<Object> multidimensionalList) {
+
+        for (Object object : multidimensionalList) {
+            if (object instanceof List)
+                flatten(object);
+            else
+                flat.add(object);
+        }
+    }
 
 }
